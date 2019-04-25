2932d685c53035a9f07948bee113e2f2874f9185
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-04-25 12:41:07 +0200

    CatalogList Component
    CatalogList Component

---
+++ src/app/features/catalog/components/catalog-list/catalog-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'modules-catalog-list',
+  template: `
+    <p>
+      catalog-list works!
+    </p>
+  `,
+  styleUrls: ['./catalog-list.component.css']
+})
+export class CatalogListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
