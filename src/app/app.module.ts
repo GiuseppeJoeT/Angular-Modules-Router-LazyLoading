c8709ed03b9d46d2a5e64f6987ab17c8bb31887d
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-04-25 12:39:55 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
+import { CatalogModule } from './features/catalog/catalog.module';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './features/login/login.module';
+import { ContactsModule } from './features/contacts/contacts.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    // componenti, direttive e pipe custom
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    CatalogModule,
+    LoginModule,
+    ContactsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
