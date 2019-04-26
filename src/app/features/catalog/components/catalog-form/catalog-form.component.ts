d74f7a633fd9abd7202f07d9ca484066ed1ff239
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-04-26 10:27:02 +0200

    Create catalog-form.component.ts

---
+++ src/app/features/catalog/components/catalog-form/catalog-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'modules-catalog-form',
+  template: `
+    <p>
+      catalog-form works!
+    </p>
+  `,
+  styleUrls: ['./catalog-form.component.css']
+})
+export class CatalogFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
