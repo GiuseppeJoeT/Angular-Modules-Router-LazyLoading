cab769d2e65414a235ce94a5c580fd6a65d92f86
Giuseppe Tiberi <tiberiweb@gmail.com>
2019-04-29 13:56:32 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { LoginComponent } from './features/login/login.component';
+import { ContactsComponent } from './features/contacts/contacts.component';
+import { CatalogComponent } from './features/catalog/catalog.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'catalog', component: CatalogComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: '**', redirectTo: 'login' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
